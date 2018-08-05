fcbefa20543a2c3207ec7a7f4882cebc851ab2c7
Rafael Baena Neto <rafael.baena@callflex.net.br>
2018-08-05 12:45:15 -0300

    Alterações para o hosts de produção

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -150,7 +150,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*.herokuapp.com']
+ALLOWED_HOSTS = ['callflexlms.herokuapp.com']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
