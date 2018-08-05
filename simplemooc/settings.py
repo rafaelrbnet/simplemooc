2c940cfc1b786c4c83389fb798faa9f4978bdbfd
Rafael Baena Neto <rafael.baena@callflex.net.br>
2018-08-05 12:38:38 -0300

    Alterações para o hosts de produção

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -137,8 +137,6 @@ CONTACT_EMAIL = 'rafael.rbnet@gmail.com'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
 DATABASES = {
     'default':  dj_database_url.config(),
 }
@@ -152,7 +150,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['callflexlms.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
